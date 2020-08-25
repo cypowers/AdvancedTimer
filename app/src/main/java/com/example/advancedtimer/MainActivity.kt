cfd8fe2feca5f8052cfea14548e05a0c035c5abb
devin <devin.park@outlook.com>
2020-08-25 10:08:16 +0900

    installed anko library. and added button to display alert message.

--- app/src/main/java/com/example/advancedtimer/MainActivity.kt
+++ app/src/main/java/com/example/advancedtimer/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.advancedtimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.alert
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_1.setOnClickListener {
+            alert("Test") {
+                title = "alert"
+            }.show()
+        }
     }
 }
